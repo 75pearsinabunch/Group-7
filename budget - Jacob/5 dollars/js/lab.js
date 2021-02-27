6fe6c13dd931acc636c7c3873273ee3b7514890d
Jhanshaw <jacob.soccer2002gmail.com>
2021-02-27 15:22:34 -0800

    Changes to budget section

--- budget - Jacob/5 dollars/js/lab.js
+++ budget - Jacob/5 dollars/js/lab.js
@@ -3,8 +3,12 @@ function togglePopup(){
   document.getElementById("popup-1").classList.toggle("active");
 };
 
-$("#main_box_1").click(){
+//appends an "active" class to .popup and .popup-content when the "Open" button is clicked
+$(".open").on("click", function() {
+  $(".popup-overlay, .popup-content").addClass("active");
+});
 
-
-
-}
+//removes the "active" class to .popup and .popup-content when the "Close" button is clicked
+$(".close, .popup-overlay").on("click", function() {
+  $(".popup-overlay, .popup-content").removeClass("active");
+});
